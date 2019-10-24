07903e39dc5dd9f719e881350fd257ab6c481a0b
uurmht <55434633+uurmht@users.noreply.github.com>
2019-10-24 15:12:10 +0300

    Added Entities

---
+++ AnkoperMuzikDans/Entity/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading;
+using System.Web;
+
+namespace AnkoperMuzikDans.Entity
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext():base("DataConnection")
+        {
+
+            Database.SetInitializer(new DataInitilializer());
+
+        }
+
+
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+    }
+    
+}
---
+++ AnkoperMuzikDans/Entity/Lecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace AnkoperMuzikDans.Entity
+{
+    public class Lecture
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        
+
+
+    }
+}
